3f7e1a701465e23a9f21e9f4b40a7d7fca5d1d48
maxstain <firaschabchoub@hotmail.com>
2024-10-03 17:09:13 +0100

    Working on the authentication.

--- lib/Services/AuthenticationServices.dart
+++ lib/Services/AuthenticationServices.dart
@@ -23,4 +23,16 @@ class AuthenticationServices {
       Fluttertoast.showToast(msg: "Failed to sign out");
     }
   }
+
+  // Register with email and password
+  Future registerWithEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential userCredential = await _firebaseAuth
+          .createUserWithEmailAndPassword(email: email, password: password);
+      signInWithEmailAndPassword(email, password);
+      return userCredential;
+    } catch (e) {
+      Fluttertoast.showToast(msg: "Failed to register");
+    }
+  }
 }
