9b236afcac5def3aefb03b5a95ac76f498af7dc2
maxstain <firaschabchoub@hotmail.com>
2024-10-03 18:58:37 +0100

    Working on the drawer and user profile + modified README.md.

--- lib/Views/Profile/profile_page.dart
+++ lib/Views/Profile/profile_page.dart
@@ -34,11 +34,55 @@ class _ProfilePageState extends State<ProfilePage> {
       body: Column(
         children: [
           Center(
-            child: Image.network(
-                FirebaseAuth.instance.currentUser?.photoURL ?? ''),
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Theme.of(context).colorScheme.secondary,
+                  width: 5,
+                ),
+                borderRadius: BorderRadius.circular(100),
+              ),
+              child: Image.network(
+                FirebaseAuth.instance.currentUser?.photoURL ??
+                    'https://st3.depositphotos.com/6672868/13801/v/450/depositphotos_138013506-stock-illustration-user-profile-group.jpg',
+                height: 200,
+                width: 200,
+                fit: BoxFit.cover,
+                loadingBuilder: (BuildContext context, Widget child,
+                    ImageChunkEvent? loadingProgress) {
+                  if (loadingProgress == null) {
+                    return child;
+                  } else {
+                    return const CircularProgressIndicator();
+                  }
+                },
+              ),
+            ),
           ),
-          Text(FirebaseAuth.instance.currentUser?.displayName ?? ''),
-          Text('Email: ${FirebaseAuth.instance.currentUser?.email}'),
+          Text(
+            FirebaseAuth.instance.currentUser?.displayName ?? 'Username',
+            style: const TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              shape: BoxShape.rectangle,
+              border: Border.all(
+                color: Theme.of(context).colorScheme.secondary,
+                width: 2,
+              ),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            padding: const EdgeInsets.all(10),
+            child: Text(
+              'Email: ${FirebaseAuth.instance.currentUser?.email}',
+              style: const TextStyle(
+                fontSize: 18,
+              ),
+            ),
+          ),
         ],
       ),
     );
